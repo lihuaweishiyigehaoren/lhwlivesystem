1e078d9ae362126e6fccc4fcc9a8d0a529e2fc22
lihuaweishiyigehaoren <1206909328@qq.com>
2019-03-18 06:17:21 -0700

    queue

--- spider/translayor/src/EventQueue.h
+++ spider/translayor/src/EventQueue.h
@@ -22,9 +22,90 @@ namespace translayor
         {
         }
 
+        void SetData(const ByteArray& data)
+        {
+            _data = data;
+        }
+
+        const ByteArray& GetData() const
+        {
+            return _data;
+        }
+
+        void SetType(const std::string& type)
+        {
+            _type = type;
+        }
+
+        const std::string& GetType() const
+        {
+            return _type;
+        }
+
+        void SetStream(IStream* stream)
+        {
+            _stream = stream;
+        }
+
+        IStream* GetStream() const
+        {
+            return _stream;
+        }
+
     private:
         std::string _type;
         ByteArray _data;
         IStream* _stream;
+    };
+
+    class EventQueue
+    {
+    public:
+        EventQueue(int32_t timeout=0) :
+        _timeout(timeout){
+
+        }
+        ~EventQueue(){}
+
+        void PostEvent(BaseEvent * event)
+        {
+            std::unique_lock<std::mutex> locker(_mutex);
+            _events.push_back(std::shared_ptr<BaseEvent>(event));
+        }
+
+        std::shared_ptr<BaseEvent> GetEvent()
+        {
+            std::unique_lock<std::mutex> locker(_mutex);
+
+            if(_events.empty())
+            {
+                if(_timeout==0)
+                {
+                    return nullptr;
+                }
+
+                _waitCondition.wait_for(locker,std::chrono::milliseconds(_timeout));// 线程等待
+            }
+
+            if(!_events.empty())
+            {
+                std::shared_ptr<BaseEvent> event = _events.front();
+                _events.erase(_events.begin());
+
+                return event;
+            }
+
+            return nullptr;
+        }
+
+
+
+    private:
+        std::vector<std::shared_ptr<BaseEvent>> _events;
+        std::mutex _mutex;
+        std::condition_variable _waitCondition;
+
+        int32_t _timeout;
+
     };
 }
---
+++ spider/translayor/src/Net.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ByteArray.h"
+
+#include <functional>
+
+class IStream {
+
+public:
+    typedef std::function<int32_t(const char* buf,int64_t size)> DataHandler;
+
+    virtual int32_t Receive(char* buffer,int32_t bufferSize,int32_t& readSize)=0;
+    virtual int32_t Send(const ByteArray & byteArray)=0;
+
+    virtual void OnData(DataHandler handler)=0;
+    virtual DataHandler GetDataHandler()=0;
+
+}
